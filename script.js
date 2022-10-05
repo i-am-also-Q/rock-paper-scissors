19df962cfc04729fbb82e9d57f8a879372a2ec47
i-am-also-Q <example@example.com>
2022-10-05 00:23:09 -0400

    change console.log to innerHTML

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Q: this is a test
+document.getElementById("output").innerHTML += "Hello World!";
 //randomly return a rock, paper, or scissors value to determine the computer opponent's moves
 //randomly generate one of three integers
 //each integer corresponds to rock, paper, or scissors, like so:
@@ -5,19 +7,19 @@
 //if the integer generated is 1, return paper
 //if the integer generated is 2, return scissors
 function getComputerChoice() {
-    let randomNum = Math.floor(Math.random() * 3);
+	let randomNum = Math.floor(Math.random() * 3);
 
-    let computerChoice;
+	let computerChoice;
 
-    if (randomNum === 0) {
-        computerChoice = "rock";
-    } else if (randomNum === 1) {
-        computerChoice = "paper";
-    } else if (randomNum === 2) {
-        computerChoice = "scissors";
-    }
+	if (randomNum === 0) {
+		computerChoice = "rock";
+	} else if (randomNum === 1) {
+		computerChoice = "paper";
+	} else if (randomNum === 2) {
+		computerChoice = "scissors";
+	}
 
-    return computerChoice;
+	return computerChoice;
 }
 
 //play a single round of RPS with two parameters: playerSelection and computerSelection
@@ -26,70 +28,95 @@ function getComputerChoice() {
 //write an else in case the player's input is invalid
 //return the results
 function getPlayerChoice() {
-    let playerChoice = prompt("Rock, Paper, or Scissors?");
-    playerChoice = playerChoice.toLowerCase();
-    return playerChoice;
+	let playerChoice = prompt("Rock, Paper, or Scissors?");
+	playerChoice = playerChoice.toLowerCase();
+	return playerChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    let result;
+	let result;
 
-    if (
-        (playerSelection === "rock" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "scissors") ||
-        (playerSelection === "scissors" && computerSelection === "rock")
-    ) {
-        result = "you lose";
-    } else if (
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper") ||
-        (playerSelection === "rock" && computerSelection === "scissors")
-    ) {
-        result = "you win";
-    } else if (playerSelection === computerSelection) {
-        result = "tie";
-    } else {
-        result = "oops";
-    }
+	if (
+		(playerSelection === "rock" && computerSelection === "paper") ||
+		(playerSelection === "paper" && computerSelection === "scissors") ||
+		(playerSelection === "scissors" && computerSelection === "rock")
+	) {
+		result = "you lose";
+	} else if (
+		(playerSelection === "paper" && computerSelection === "rock") ||
+		(playerSelection === "scissors" && computerSelection === "paper") ||
+		(playerSelection === "rock" && computerSelection === "scissors")
+	) {
+		result = "you win";
+	} else if (playerSelection === computerSelection) {
+		result = "tie";
+	} else {
+		result = "oops";
+	}
 
-    return result;
+	return result;
 }
 
 //calls the playRound function to play five rounds while keeping score and then reports a winner at the end of the game
 function game() {
-    let player = 0;
-    let computer = 0;
-    let playerChoice;
-    let computerChoice;
+	let player = 0;
+	let computer = 0;
+	let playerChoice;
+	let computerChoice;
 
-    for (let rounds = 0; rounds < 5; rounds++) {
-        playerChoice = getPlayerChoice();
-        computerChoice = getComputerChoice();
+	for (let rounds = 0; rounds < 5; rounds++) {
+		playerChoice = getPlayerChoice();
+		computerChoice = getComputerChoice();
 
-        console.log(computerChoice);
+		document.getElementById("round").innerHTML += " " + (rounds + 1);
 
-        let result = playRound(playerChoice, computerChoice);
-        console.log(result);
+		document.getElementById("ai").innerHTML += computerChoice + " ";
+		document.getElementById("player").innerHTML += playerChoice + " ";
 
-        if (result === "you lose") {
-            computer++;
-        } else if (result === "you win") {
-            player++;
-        } else if (result === "tie") {
-            computer++;
-            player++;
-        }
-    }
+		let result = playRound(playerChoice, computerChoice);
+		document.getElementById("result").innerHTML +=
+			"<br> Round " +
+			(rounds + 1) +
+			" [ " +
+			result +
+			" ] <br>" +
+			"You | AI<br>" +
+			playerChoice +
+			" | " +
+			computerChoice +
+			"<br>";
 
-    console.log(`Player: ${player} \nComputer: ${computer}`);
+		if (result === "you lose") {
+			computer++;
+		} else if (result === "you win") {
+			player++;
+		} else if (result === "tie") {
+			computer++;
+			player++;
+		}
+	}
 
-    if (player > computer) {
-        console.log("You've won the game! Congrats!");
-    } else if (player < computer) {
-        console.log("I'm sorry, but you've lost the game. Try again.");
-    } else {
-        console.log("It's a tie!");
-    }
+	document.GetElementById("misc").innerHTML =
+		"Player: ${player} \nComputer: ${computer}";
+
+	if (player > computer) {
+		document.GetElementById("output").innerHTML =
+			"You've won the game! Congrats!";
+	} else if (player < computer) {
+		document.GetElementById("output").innerHTML =
+			"I'm sorry, but you've lost the game. Try again.";
+	} else {
+		document.GetElementById("output").innerHTML = "It's a tie!";
+	}
+	if (player > computer) {
+		document.GetElementById("output").innerHTML =
+			"You've won the game! Congrats!";
+	} else if (player < computer) {
+		document.GetElementById("output").innerHTML =
+			"I'm sorry, but you've lost the game. Try again.";
+	} else {
+		document.GetElementById("output").innerHTML = "It's a tie!";
+	}
 }
 
 game();
